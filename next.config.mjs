78946b5ca2f2db177ba96eff708c24923d99ce52
rookiecol <juan.diegoam@hotmail.com>
2024-12-03 14:00:03 +0700

    wip: metadata commented and turbopack setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,22 @@
-import { PHASE_PRODUCTION_BUILD } from "next/constants.js";
+  import { PHASE_PRODUCTION_BUILD } from "next/constants.js";
 
-export default function nextConfig(phase) {
-  const isBuild = phase === PHASE_PRODUCTION_BUILD;
+  export default function nextConfig(phase) {
+    const isBuild = phase === PHASE_PRODUCTION_BUILD;
 
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    webpack: (config) => {
-      if (isBuild) {
-        config.externals.push("pino-pretty", "lokijs", "encoding");
-      }
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+      experimental: {
+        turbo: true,
+      },
+      reactStrictMode: true, 
+      swcMinify: true,      
+    };
+  
+    if (isBuild) {
+      nextConfig.compiler = {
+        removeConsole: true, 
+      };
+    }
 
-      config.module.rules.push(
-        {
-          test: /\.d\.ts$/,
-          use: "ignore-loader",
-        },
-        {
-          test: /\.d\.ts\.map$/,
-          use: "ignore-loader",
-        },
-        {
-          test: /\.js\.map$/,
-          use: "ignore-loader",
-        },
-      );
-
-      return config;
-    },
-  };
-
-  return nextConfig;
-}
+    return nextConfig;
+  }
