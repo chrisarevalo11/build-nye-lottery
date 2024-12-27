1c729bfecbd9808464425c706454d25a5c48a05a
chrisarevalodev <christian.arevalo@ext.rootstocklabs.com>
2024-12-27 22:18:17 +0700

    added network on dynamic config

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,16 +1,14 @@
 import { CHAIN } from "@/config";
-import { baseSepolia } from "viem/chains";
 import { cookieStorage, createConfig, createStorage, http } from "wagmi";
 
 export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
 if (!projectId) throw new Error("WalletConnect Project ID is not defined");
 
 export const wagmiConfig = createConfig({
-  chains: [CHAIN, baseSepolia],
+  chains: [CHAIN],
   ssr: true,
   transports: {
     [CHAIN.id]: http(),
-    [baseSepolia.id]: http(),
   },
   storage: createStorage({
     storage: cookieStorage,
